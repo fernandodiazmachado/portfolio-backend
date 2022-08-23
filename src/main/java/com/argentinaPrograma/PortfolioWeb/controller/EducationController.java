ca2a71667a69d0576ceba16d251415c478ec4b38
Fernando Emanuel Díaz Machado <fernandodiazmachado@gmail.com>
2022-08-22 21:38:05 -0300

    test education and experience

--- src/main/java/com/argentinaPrograma/PortfolioWeb/controller/EducationController.java
+++ src/main/java/com/argentinaPrograma/PortfolioWeb/controller/EducationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/education")
-@CrossOrigin(origins={"http://localhost:4200","https://diaz-machado.web.app/portfolio/education"})
+@CrossOrigin(origins={"http://localhost:4200","https://diaz-machado.web.app/portfolio"})
 public class EducationController {
 
     @Autowired
--- src/main/java/com/argentinaPrograma/PortfolioWeb/controller/ExperienceController.java
+++ src/main/java/com/argentinaPrograma/PortfolioWeb/controller/ExperienceController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experience")
-@CrossOrigin(origins={"http://localhost:4200","https://diaz-machado.web.app/portfolio/experience"})
+@CrossOrigin(origins={"http://localhost:4200","https://diaz-machado.web.app"})
 public class ExperienceController {
 
     @Autowired
